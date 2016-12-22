f3c2da06c38ad709ad7c5f54471cb28cb5e6f383
James Andrews <james@openbridge.com>
2016-12-22 19:15:27 +0900

    basic functionality working

--- js/main.app.js
+++ js/main.app.js
@@ -3,10 +3,11 @@ var app = angular.module('thswebsite', [
   'ngRoute'
 ]);
 
-app.controller("RootController", ['$window','$location', function($window, $location)
+app.controller("RootController", ['$window','$location','Meetup', function($window, $location, Meetup)
 {
   self = this;
   self.lang = $window.navigator.language || $window.navigator.userLanguage;
+  self.events = [];
 
   if(self.lang == 'ja-JP')
   {
@@ -19,8 +20,60 @@ app.controller("RootController", ['$window','$location', function($window, $loca
     $location.path('/en')
   }
 
-  
-  console.log("Getting events");
+  self.getEvents = function()
+  {
+    Meetup.getEvents()
+      .then(function success(response)
+      {
+        response.forEach(function(val, idx)
+        {
+          self.events.push(val);
+        });
+
+        console.log(response);
+      }, function error(error)
+      {
+        console.log(error);
+      });
+  }
+
+  self.getEvents();
   
 }]);
+
+app.factory('Meetup', function ($http) {
+  var events = [];
+
+  return {
+    getEvents: function()
+    {
+      if(events.length == 0)
+      {
+        return $http.get('/events/meetup.php')
+          .then(function success(response)
+          {
+            if (typeof response.data === 'object')
+            {
+              response.data.forEach(function(val, idx)
+              {
+                events.push(val);
+              });
+
+              return events;
+            }
+            else
+            {
+              // something went wrong
+              return $q.reject(response.data);
+            }
+          }, function error(error)
+          {
+            // something went wrong
+            return $q.reject(response.data);
+          });
+      };
+    },
+  }
+});
+
 
--- js/routes.app.js
+++ js/routes.app.js
@@ -6,6 +6,27 @@ window.version=20161211
 app.config( ['$routeProvider', function($routeProvider)
 {
   $routeProvider
+    .when('/en/faq', {
+      templateUrl: 'templates/english/faq.html?version=' + window.siteVersion,
+      access: {
+        cssClass: 'page-roadmap',
+        title: "Access - Tokyo Hackerspace"
+      }
+    })
+    .when('/en/events', {
+      templateUrl: 'templates/english/events.html?version=' + window.siteVersion,
+      access: {
+        cssClass: 'page-roadmap',
+        title: "Access - Tokyo Hackerspace"
+      }
+    })
+    .when('/en/about', {
+      templateUrl: 'templates/english/about.html?version=' + window.siteVersion,
+      access: {
+        cssClass: 'page-roadmap',
+        title: "Access - Tokyo Hackerspace"
+      }
+    })
     .when('/en/access', {
       templateUrl: 'templates/english/access.html?version=' + window.siteVersion,
       access: {
@@ -19,6 +40,34 @@ app.config( ['$routeProvider', function($routeProvider)
       access: {
         footer: true,
         title: "Tokyo Hackerspace"
+      }
+    })
+    .when('/ja/faq', {
+      templateUrl: 'templates/japanese/faq.html?version=' + window.siteVersion,
+      access: {
+        cssClass: 'page-roadmap',
+        title: "Access - Tokyo Hackerspace"
+      }
+    })
+    .when('/ja/events', {
+      templateUrl: 'templates/japanese/events.html?version=' + window.siteVersion,
+      access: {
+        cssClass: 'page-roadmap',
+        title: "Access - Tokyo Hackerspace"
+      }
+    })
+    .when('/ja/about', {
+      templateUrl: 'templates/japanese/about.html?version=' + window.siteVersion,
+      access: {
+        cssClass: 'page-roadmap',
+        title: "Access - Tokyo Hackerspace"
+      }
+    })
+    .when('/ja/access', {
+      templateUrl: 'templates/japanese/access.html?version=' + window.siteVersion,
+      access: {
+        cssClass: 'page-roadmap',
+        title: "Access - Tokyo Hackerspace"
       }
     })
     .when('/ja',
