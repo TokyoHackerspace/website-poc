6655868d4f5bcb3b412f74bbbebac539b25801c4
James Andrews <james@openbridge.com>
2016-12-23 20:27:35 +0900

    fixed language selector so it stays on the same page just different language

--- js/main.app.js
+++ js/main.app.js
@@ -11,21 +11,40 @@ app.controller("RootController", ['$scope', '$window','$location','Meetup', func
   self = this;
   self.lang = $window.navigator.language || $window.navigator.userLanguage;
   self.events = [];
-  
+  self.baseUrlPath = "en"
+  self.languageLinks = {
+    en: {
+      base: "/en",
+      url: ""
+    },
+    ja: {
+      base: "/ja",
+      url: ""
+    }
+  }
+
+  // When we change a route we're going to do some work.
   $scope.$on('$routeChangeStart', function (e, next, current)
   {
-    if(next.$$route.originalPath == "/")
+    // Get the original path
+    self.originalPath = next.$$route.originalPath;
+
+    // if not the absolute base path change the languageLinkUrls
+    if(self.originalPath != "/")
+    {
+      pathSuffix = self.originalPath.substring(3);
+      self.languageLinks.en.url = self.languageLinks.en.base + pathSuffix;
+      self.languageLinks.ja.url = self.languageLinks.ja.base + pathSuffix;
+    }
+    
+    // Redirect to the right language website
+    if(self.originalPath == "/")
     {
       if(self.lang == 'ja-JP')
       {
-        console.log("Redirecting to Japanese website");
-        $location.path('/ja')
+        baseUrlPath='ja';
       }
-      else
-      {
-        console.log("Redirecting to English website");
-        $location.path('/en')
-      }
+        $location.path('/' + baseUrlPath)
     }
   });
 
@@ -44,9 +63,10 @@ app.controller("RootController", ['$scope', '$window','$location','Meetup', func
       });
   }
 
-  self.getEvents();
+  // self.getEvents();
   
 }]);
+
 
 app.factory('Meetup',['$http','$sce', function ($http, $sce) {
   var events = [];
