e5dedf01516932c258fca7970931c8f15e5c53b3
James Andrews <james@openbridge.com>
2017-03-15 11:02:21 +0900

    removed unneeded code

--- assets/js/ths/main.app.js
+++ assets/js/ths/main.app.js
@@ -12,7 +12,6 @@ window.mapContents = null;
 // Define obconsole angular module.
 var app = angular.module('thswebsite', [
   'ngRoute',
-  'oc.lazyLoad'
 ]);
 
 app.controller("RootController", ['$scope', '$window','$location', function($scope, $window, $location)
@@ -71,58 +70,3 @@ app.controller("EventDisplayController", ['$scope', '$routeParams','$window', fu
   self.eventId = $routeParams.eventId;
   self.eventTitle = $routeParams.eventTitle;
 }]);
-
-
-app.controller('MapController',['$scope', '$document','$ocLazyLoad', function($scope, $document,$ocLazyLoad)
-{
-  // Loading the map api for google using LazyLoader.  It then calls the callback function initMap which lives outside
-  // Of angulage.
- $ocLazyLoad.load('https://maps.googleapis.com/maps/api/js?key=AIzaSyDZDTW0XaQ3Qd3RVFtvGpiziwEQwGasEA4&callback=initMap')
-
- if(window.mapContents != null)
- {
-   var directionsService = new google.maps.DirectionsService;
-   var directionsDisplay = new google.maps.DirectionsRenderer;
-   
-   directionsDisplay.setMap(window.mapContents);
-   calculateAndDisplayRoute(directionsService, directionsDisplay);
-   // calculateAndDisplayRoute(directionsService, directionsDisplay);
-   // document.getElementById('map').innerHTML = ;
-   // console.log("Have map contents");
- }
-
-
-}]);
-
-
-// This is the callback function for the Google Map API.  It must live outside of Angular.
-function initMap()
-{
-  
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: 35.7562022, lng: 139.6924972, zoom: 17}
-  });
-  window.mapContents = map;
-  directionsDisplay.setMap(map);
-  
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-
-}
-
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-  directionsService.route({
-    origin: "Nakaitabashi Station",
-    destination: "173-0021 Tokyo-to, Itabashi-ku, Yayoichō, 40−7",
-    travelMode: 'WALKING'
-  }, function(response, status) {
-    if (status === 'OK') {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-  
-}
